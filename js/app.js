a37dfc56b1974672e4eb9c84d6aac9cb6a2903aa
Madhav Narayan Khullar <madhavnarayanwebd@gmail.com>
2020-07-31 06:08:29 +0530

    update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-	 const links=document.querySelector(".alternative-styls"),
+	 const links=document.querySelectorAll(".alternative-styls"),
 	 totallinks=links.length;
+	 console.log(links)
  function setActiveStyle(color) {
+ 	console.log(links)
  	for(let i=0;i<totallinks;i++)
  	{
  		if(color == links[i].getAttribute("title")){
  			links[i].removeAttribute("disabled");
+ 		}
+ 		else{
+ 			links[i].setAttribute("disabled","true");
  		}
  	}
  	
